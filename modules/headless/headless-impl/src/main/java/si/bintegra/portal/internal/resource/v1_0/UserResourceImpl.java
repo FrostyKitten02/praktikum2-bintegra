a005499ce4f3f0e63b6bf3d73df32db67bc0b155
FrostyKitten02 <alen.fridau02@gmail.com>
2023-05-15 00:02:22 +0200

    create new foo example

--- modules/headless/headless-impl/src/main/java/si/bintegra/portal/internal/resource/v1_0/UserResourceImpl.java
+++ modules/headless/headless-impl/src/main/java/si/bintegra/portal/internal/resource/v1_0/UserResourceImpl.java
@@ -1,9 +1,14 @@
 package si.bintegra.portal.internal.resource.v1_0;
 
+import com.liferay.counter.kernel.service.CounterLocalService;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
+import si.bintegra.model.Foo;
 import si.bintegra.portal.dto.v1_0.User;
 import si.bintegra.portal.resource.v1_0.UserResource;
+import si.bintegra.service.FooLocalService;
+
 
 /**
  * @author Alen
@@ -13,9 +18,21 @@ import si.bintegra.portal.resource.v1_0.UserResource;
 	scope = ServiceScope.PROTOTYPE, service = UserResource.class
 )
 public class UserResourceImpl extends BaseUserResourceImpl {
+
+
+
+	@Reference
+	private FooLocalService fooLocalService;
+	@Reference
+	private CounterLocalService counterLocalService;
+
 	@Override
 	public User getTestPath() throws Exception {
 		User user = new User();
+		long newId = counterLocalService.increment();
+		Foo foo = fooLocalService.createFoo(newId);
+		foo.setField5("test");
+		fooLocalService.addFoo(foo);
 		user.setId(1);
 		user.setName("Klara");
 		return user;
