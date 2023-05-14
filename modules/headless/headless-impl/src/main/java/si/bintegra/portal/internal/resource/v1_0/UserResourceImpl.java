d63f791de2badce6aefaaedb11894b616115043c
FrostyKitten02 <alen.fridau02@gmail.com>
2023-05-14 19:42:32 +0200

    init service

--- modules/headless/headless-impl/src/main/java/si/bintegra/portal/internal/resource/v1_0/UserResourceImpl.java
+++ modules/headless/headless-impl/src/main/java/si/bintegra/portal/internal/resource/v1_0/UserResourceImpl.java
@@ -2,7 +2,6 @@ package si.bintegra.portal.internal.resource.v1_0;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.ServiceScope;
-
 import si.bintegra.portal.dto.v1_0.User;
 import si.bintegra.portal.resource.v1_0.UserResource;
 
